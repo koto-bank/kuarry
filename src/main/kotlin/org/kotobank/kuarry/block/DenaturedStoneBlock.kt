930bcc674c2007060d0e58a457b2a66d14d0d61f
Ekaterina Vaartis <vaartis@cock.li>
2019-11-22 10:21:04 +0300

    Add a tooltip to the denatured stone block

--- src/main/kotlin/org/kotobank/kuarry/block/DenaturedStoneBlock.kt
+++ src/main/kotlin/org/kotobank/kuarry/block/DenaturedStoneBlock.kt
@@ -3,11 +3,15 @@ package org.kotobank.kuarry.block
 import net.minecraft.block.Block
 import net.minecraft.block.material.Material
 import net.minecraft.block.state.IBlockState
+import net.minecraft.client.util.ITooltipFlag
 import net.minecraft.init.Blocks
 import net.minecraft.item.ItemStack
 import net.minecraft.util.NonNullList
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.IBlockAccess
+import net.minecraft.world.World
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
 import org.kotobank.kuarry.KuarryMod
 import java.util.Random
 
@@ -29,5 +33,14 @@ class DenaturedStoneBlock(material: Material, registryName: String) : Block(mate
         if (random.nextInt(5) == 0) {
             drops.add(ItemStack(Blocks.GRAVEL))
         }
+    }
+
+    @SideOnly(Side.CLIENT)
+    override fun addInformation(stack: ItemStack, player: World?, tooltip: MutableList<String>, advanced: ITooltipFlag) {
+        tooltip.add(
+                "Denatured stone is proven to be a very useful material: it can be a cheap papier mache, " +
+                        "it can be smashed to pieces, or it can be the subject of a ten year long \"How can we use denatured stone?\" research with an immense budget. " +
+                        "Most importantly, however, is that it can sometimes drop gravel when broken."
+        )
     }
 }
